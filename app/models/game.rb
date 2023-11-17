dabccc2fd2e22092e9700895e1c83320a657c098
authorbeard <msnolan@gmail.com>
2023-11-17 13:45:15 -0800

    first test for Game.generate_new: acquire coordinates. Use Faker to generate lat & lng for game

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,5 +5,12 @@ class Game < ApplicationRecord
   after_validation :reverse_geocode if Proc.new { longitude_changed? || latitude_changed? }
 
   def self.generate_new 
+    coordinates = generate_coordinates
+  end
+
+  # NOTE: Implementation seems likely to change in the future, so I'm moving coordinate generation
+  # here; all the generation really cares about right now is that it gets some coordinates.
+  def self.generate_coordinates
+    { latitude: Faker::Address.latitude, longitude: Faker::Address.longitude }
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -12,5 +12,16 @@ RSpec.describe Game do
       expect(game.save!).to be true 
       expect(game.reload.address).not_to be_nil
     end
+
+    describe ".generate_new" do 
+      it "generates random coordinates" do 
+        game = build(:game)
+        allow(Game).to receive(:generate_coordinates).and_call_original
+
+        Game.generate_new
+
+        expect(Game).to have_received(:generate_coordinates)
+      end
+    end
   end
 end
