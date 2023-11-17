09eb1f71f0d1608b3d060331b9ba66abc1532f2e
authorbeard <msnolan@gmail.com>
2023-11-17 12:05:27 -0800

    validate that user exists at Games#create. Add test

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,14 @@
 class GamesController < ApplicationController
+  before_action :validate_user
+
   def create
+  end
+
+  private 
+
+  def validate_user
+    if current_user.nil?
+      render json: { error: 'Not found; post your email to /users to create a user' }, status: 422 and return 
+    end
   end
 end
--- spec/requests/games_request_spec.rb
+++ spec/requests/games_request_spec.rb
@@ -9,5 +9,12 @@ RSpec.describe GamesController, type: :request do
 
       expect(response).to have_http_status(422)
     end
+
+    it 'responds with instructions if user not found' do 
+      post games_path(email: 'whatever@example.com')
+
+      expect(response).not_to be_successful
+      expect(response.body).to include('Not found; post your email to /users to create a user')
+    end
   end
 end
