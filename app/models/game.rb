a55d56beb9be406a6428e370d690b4e7f55af59f
authorbeard <msnolan@gmail.com>
2023-11-17 13:38:16 -0800

    finally generate Geocoder config. Set up to config linear distances in km. Switch callback in Game to after_validation; before_save was running into db non-null constraint on address for some reason

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,8 @@
 class Game < ApplicationRecord
   validates :latitude, :longitude, presence: true
-  
+
   reverse_geocoded_by :latitude, :longitude
-  before_validation :reverse_geocode if Proc.new { longitude_changed? || latitude_changed? }
+  after_validation :reverse_geocode if Proc.new { longitude_changed? || latitude_changed? }
 
   def self.generate_new 
   end
